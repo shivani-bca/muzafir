17320a2f6af566445ef6facd95256205a48a1d2b
shivani-bca <shivanikumarisk0000@gmail.com>
2025-07-10 09:47:03 +0530

    Installed @tailwindcss/vite and fixed config

--- src/assets/component/Footer.jsx
+++ src/assets/component/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     <footer className="bg-gradient-to-r from-rose-100 via-pink-200 to-rose-100 text-center py-6 mt-10 shadow-inner">
       <div className="flex flex-col sm:flex-row justify-center items-center gap-4 text-sm text-gray-600 font-poppins">
         <p>
-          © 2025 <span className="font-playfair text-rose-600 font-semibold">xyz</span> — Crafted with elegance in Paris 🗼
+          © 2025 <span className="text-xl sm:text-2xl font-playfair  bg-gradient-to-r from-rose-500  via-amber-400 to-pink-500  text-transparent bg-clip-text font-bold tracking-wide">
+        Muzafir</span> — Crafted with elegance in Paris 🗼
         </p>
         <p className="hidden sm:inline-block">|</p>
         <p>
--- src/assets/component/Navbar.jsx
+++ src/assets/component/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="bg-gradient-to-r from-rose-100 via-rose-200 to-rose-100 shadow-md px-5 py-1 flex  sticky top-0 justify-between items-center">
-      <h1 className="text-xl sm:text-2xl font-playfair font-bold text-transparent bg-clip-text bg-gradient-to-r from-rose-300 via-pink-500 to-rose-800 tracking-wide">
-        xyz
+      <h1 className="text-xl sm:text-2xl font-playfair  bg-gradient-to-r from-rose-500  via-amber-400 to-pink-500  text-transparent bg-clip-text font-bold tracking-wide">
+        Muzafir
       </h1>
       <div className="space-x-5 font-poppins text-gray-700 text-sm">
         <a href="#" className="hover:text-rose-700">Home</a>
